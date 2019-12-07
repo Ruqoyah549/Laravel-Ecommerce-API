006b8d78fdfe70a63fcb9266ccc52bee95b9254d
Bisola Tiamiyu <bisola.tiamiyu@feghas.com>
2019-12-07 03:52:28 +0100

    Review Created

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers;
 use App\Model\Review;
 use App\Model\Product;
 use App\Http\Resources\ReviewResource;
+use App\Http\Requests\ReviewRequest;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class ReviewController extends Controller
 {
@@ -21,9 +24,15 @@ class ReviewController extends Controller
     }
 
 
-    public function store(Request $request)
+    public function store(ReviewRequest $request, Product $product)
     {
-        //
+        $review = new Review($request->all());
+        $product->reviews()->save($review);
+
+        return response([
+            'data' => new ReviewResource($review)
+        ], Response::HTTP_CREATED);
+
     }
 
 
